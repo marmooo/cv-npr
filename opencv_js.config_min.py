172a708b22293d3b2be4e5f097d35ee81c6d3753
marmooo <marmooo@users.noreply.github.com>
2025-04-07 08:52:04 +0900

    improve build script

--- opencv_js.config_min.py
+++
@@ -1,7 +0,0 @@
-imgproc = {"": ["cvtColor"]}
-photo = {"": ["detailEnhance", "edgePreservingFilter", "pencilSketch", "stylization"]}
-# xphoto = {"": ["oilPainting"]}
-# ximgproc = {"": ["dtFilter"]}
-
-# white_list = makeWhiteList([imgproc, photo, xphoto, ximgproc])
-white_list = makeWhiteList([imgproc, photo])
